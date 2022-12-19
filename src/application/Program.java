26b33af870eaa495bc5f7cd3b9f87a26e2d71da9
Rafael Couto <rafaelscouto@gmail.com>
2022-12-19 10:47:16 -0300

    Create the first class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.print("Hello World");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
---
+++ src/boardGame/Position.java
@@ -0,0 +1,37 @@
+package boardGame;
+
+public class Position {
+
+	private Integer row;
+	private Integer column;
+	
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+	
+	public void setValues(int row, int column) {
+		
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+}
